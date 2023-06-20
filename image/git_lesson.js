bf684a862a91f94ee182bb5aea22b9caa3b87a3d
Đức Vũ <ducvu2509@gmail.com>
2023-06-20 13:33:40 +0700

    add code

---
+++ image/git_lesson.js
@@ -0,0 +1,15 @@
+/**
+ *  1. フォルダーを作成
+ *  2. staging area
+ *  3. commit ( Repository )
+ * 
+ * https://backlog.com/git-tutorial/vn/
+ * 
+ *  Nhóm thẻ block là những nhóm thẻ chiếm hết chiều ngang của trang web
+ *  　 + width, height, margin, paddingを設定する可能性があります。
+ *     + 例：div, p, secsion, aside, article, header, footer,..
+ *  Nhóm thẻ inline:
+ *   + Là những thẻ năm trên cùng 1 hàng, 
+ *   + width, heightを設定できなく, margin paddingの左右しか設定しない
+ *   + 例：　input, span, img, lable, button,..
+ *  */
